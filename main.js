56a12560cd1f34c0c66661977f758233bfc3a76a
William Milanez <william_milanez@hotmail.com>
2024-09-30 21:59:10 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,76 @@
+// variáveis
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+
+let randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+
+const maximumAttempts = 
+
+
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', pressEnter)
+
+// funções callback
+function handleTryClick(event) {
+    event.preventDefault() // não faça o padrão deste evento, não envie o formulário
+
+    const inputNumber = document.querySelector("#inputNumber")
+
+    if(Number(inputNumber.value) == randomNumber) {
+        toggleScreen()
+
+        screen2.querySelector("h2").innerText = `Parabéns! O número é escondido é o ${randomNumber} e você acertou em ${xAttempts} tentativas!`
+        // document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas!`
+
+
+
+    }   else if(inputNumber.value == maximumAttempts) {
+            toggleScreen()
+            
+            screen2.querySelector("h2").innerText = `Infelizmente acabaram as suas tentativas! O número correto era o ${randomNumber}!`
+    
+
+
+
+
+    }   else if(Number(inputNumber.value) < 0) {
+            alert("ATENÇÃO: O número digitado NÃO PODE SER MENOR do que 0!")
+    }   else if(Number(inputNumber.value) > 10) {
+            alert("ATENÇÃO: O número digitado NÃO PODE SER MAIOR do que 10!")
+    }   else if(inputNumber.value == "") {
+            alert("ATENÇÃO: Não foi informado nenhum número!")
+    }   else if(inputNumber.value < randomNumber) {
+            alert("DICA: Tente um número MAIOR!")
+    }   else if(inputNumber.value > randomNumber) {
+            alert("DICA: Tente um número MENOR!")
+    }
+
+    inputNumber.value = ""
+    xAttempts++
+}
+
+function handleResetClick() {
+    toggleScreen()
+    xAttempts = 1
+    randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen() {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
+
+function pressEnter(e) {
+    if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+        handleResetClick()
+        }
+}
+
+
+
